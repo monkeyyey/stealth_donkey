389f56b51a64f7c90ce0098201c84277ddd82416
monkeyyey <78858140+monkeyyey@users.noreply.github.com>
2022-04-09 00:02:01 +0800

    Update DatabaseConfig.js

--- Models/DatabaseConfig.js
+++ Models/DatabaseConfig.js
@@ -6,7 +6,7 @@ var dbconnect = {
             host: "localhost",
             port: 3306,
             user: "root",
-            password: "wobuai88",
+            password: "",
             database: "agent",
             // retain date as a string
             dateStrings: true,
